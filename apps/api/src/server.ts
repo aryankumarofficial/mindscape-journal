f58e669c93e729f4a62b312ae2027541da2f8a2a
Aryan Kumar <aryanak9163@gmail.com>
2026-03-13 01:55:52 +0530

    feat: adds the health checkpoint

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -4,10 +4,30 @@ const app = express();
 
 app.use(express.json());
 
+const port = process.env.PORT
+
 app.get("/", (_, res) => {
-  res.send("Mindscape API running");
+  res.json({
+    message: "API Server Running successfully",
+    info:`check health checkpoint at /health`
+  });
 });
 
-app.listen(4000, () => {
-  console.log("API running on port 4000");
+
+app.get("/health", (_req, res) => {
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime()>=60?process.uptime()>=3600?`${process.uptime()/3600} HRS ${process.uptime()%3600} MIN ${process.uptime()%60} SEC`:`${process.uptime()/60} MIN ${process.uptime()%60} SEC`:`${process.uptime()} SEC`,
+    timestamp: new Date(Date.now())
+  });
 });
+
+const server = app.listen(port, () => {
+  const address = server.address();
+  if (typeof address === "object" && address) {
+    const host = address.address === "::" ? "localhost" : address.address;
+    const port = address.port;
+
+    console.log(`API running at http://${host}:${port}`)
+  }
+})
