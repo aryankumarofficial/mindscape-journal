0e28a112e93ebd0fc5268b63824ef7e16bbde3fc
Aryan Kumar <aryanak9163@gmail.com>
2026-03-13 04:36:04 +0530

    feat: configures the controller with routes for register and login

---
+++ apps/api/src/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from "express";
+import { validate } from "../middlewares/validate.middleware";
+import { loginSchema, registerSchema } from "../validators/auth.schema";
+import { login, register } from "../controllers/auth.controller";
+
+const router = Router();
+
+router.post("/register", validate(registerSchema), register);
+
+router.post("/login", validate(loginSchema), login);
+
+export default router;
--- apps/api/src/routes/index.ts
+++ apps/api/src/routes/index.ts
@@ -1,11 +1,25 @@
 import { Router } from "express";
 
 import authRoutes from "./auth.routes";
-import userRoutes from "./user.routes";
 
 const router = Router();
 
+router.get("/", (_, res) => {
+  res.json({
+    message: "API Server Running successfully",
+    info:`check health checkpoint at /health`
+  });
+});
+
+
+router.get("/health", (_req, res) => {
+  res.status(200).json({
+    status: "ok",
+    uptime: process.uptime()>=60?process.uptime()>=3600?`${process.uptime()/3600} HRS ${process.uptime()%3600} MIN ${process.uptime()%60} SEC`:`${process.uptime()/60} MIN ${process.uptime()%60} SEC`:`${process.uptime()} SEC`,
+    timestamp: new Date(Date.now())
+  });
+});
+
 router.use("/auth", authRoutes);
-router.use("/users", userRoutes);
 
 export default router;
